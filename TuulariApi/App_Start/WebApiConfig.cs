550f25ef26d5c541706911f251ee53ceb1e3e246
Jussi Heikkinen <jussih91@gmail.com>
2015-11-30 10:58:07 +0200

    Update WebApiConfig.cs

--- TuulariApi/App_Start/WebApiConfig.cs
+++ TuulariApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,15 +13,16 @@ namespace TuulariApi
             var json = config.Formatters.JsonFormatter;
             json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
             config.Formatters.Remove(config.Formatters.XmlFormatter);
-
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Routes.MapHttpRoute("DefaultApiWithId", "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("DefaultApiWithAction", "api/{controller}/{action}");
+            /* alkuperäinen
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            );*/
         }
     }
 }
